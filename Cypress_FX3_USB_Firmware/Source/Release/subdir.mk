6df05352512d5aacdf4816bff713b8bf824de4dc
vectology-yuichi <yyamada@vectology.jp>
2024-12-03 12:07:00 +0900

    [update] EZ USB Suiteプロジェクトファイル化
    * Releaseビルドのみ設定した。Debugはそれをコピーして後ほど作る
    * まだパス解決がうまくいかずビルドできない状態

---
+++ Cypress_FX3_USB_Firmware/Source/Release/subdir.mk
@@ -0,0 +1,56 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../camera_ptzcontrol.c \
+../cyfxtx.c \
+../cyfxuvcdscr.c \
+../sensor_imx219.c \
+../sensor_imx290.c \
+../sensor_imx477.c \
+../uvc.c 
+
+S_UPPER_SRCS += \
+../cyfx_gcc_startup.S 
+
+OBJS += \
+./camera_ptzcontrol.o \
+./cyfx_gcc_startup.o \
+./cyfxtx.o \
+./cyfxuvcdscr.o \
+./sensor_imx219.o \
+./sensor_imx290.o \
+./sensor_imx477.o \
+./uvc.o 
+
+C_DEPS += \
+./camera_ptzcontrol.d \
+./cyfxtx.d \
+./cyfxuvcdscr.d \
+./sensor_imx219.d \
+./sensor_imx290.d \
+./sensor_imx477.d \
+./uvc.d 
+
+S_UPPER_DEPS += \
+./cyfx_gcc_startup.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+%.o: ../%.c
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM C Compiler'
+	arm-none-eabi-gcc -mcpu=arm926ej-s -marm -mthumb-interwork -O3 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g -D__CYU3P_TX__=1 -I"C:\Program Files (x86)\Cypress\EZ-USB FX3 SDK\1.3\/fw_lib/1_3_3/inc" -std=gnu11 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+%.o: ../%.S
+	@echo 'Building file: $<'
+	@echo 'Invoking: Cross ARM GNU Assembler'
+	arm-none-eabi-gcc -mcpu=arm926ej-s -marm -mthumb-interwork -O3 -fmessage-length=0 -fsigned-char -ffunction-sections -fdata-sections -Wall  -g -x assembler-with-cpp -I"C:\Program Files (x86)\Cypress\EZ-USB FX3 SDK\1.3\/fw_lib/1_3_3/inc" -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" -c -o "$@" "$<"
+	@echo 'Finished building: $<'
+	@echo ' '
+
+
